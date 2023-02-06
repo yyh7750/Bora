e4881d71c49f1c22152985555e9cc432a51e1069
2sseul <iseul0626@gmail.com>
2023-02-06 17:55:54 +0900

    feat: 뭐했을까요

--- Frontend/src/UI/NavBar/NavBar.js
+++ Frontend/src/UI/NavBar/NavBar.js
@@ -13,18 +13,17 @@ export default function Navbar() {
 
   const showSidebar = () => setSidebar(!sidebar);
 
-  // useEffect(() => {
-  //   document.body.style.cssText = `
-  //     position: fixed;
-  //     top: -${window.scrollY}px;
-  //     overflow-y: scroll;
-  //     width: 100%;`;
-  //   return () => {
-  //     const scrollY = document.body.style.top;
-  //     document.body.style.cssText = "";
-  //     window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
-  //   };
-  // }, []);
+  if (sidebar) {
+    document.body.style.cssText = `
+    position: fixed;
+    top: -${window.scrollY}px;
+    overflow-y: scroll;
+    width: 100%;`;
+  } else {
+    const scrollY = document.body.style.top;
+    document.body.style.cssText = "";
+    window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+  }
 
   return (
     <>
