e4b0102d12e1a6bf7d89d2c72cf3753471679900
이상민 <lsms3723@daum.net>
2023-02-15 09:56:00 +0900

    feat: jasypt 추가

--- src/test/java/com/ssafy/bora/BoraApplicationTests.java
+++ src/test/java/com/ssafy/bora/BoraApplicationTests.java
@@ -1,13 +1,46 @@
 package com.ssafy.bora;
 
+import org.assertj.core.api.Assertions;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BoraApplicationTests {
 
     @Test
-    void contextLoads() {
+    void jasypt(){
+        String url = "";
+        String username = "";
+        String password = "";
+
+        String encryptUrl = jasyptEncrypt(url);
+        String encryptUsername = jasyptEncrypt(username);
+        String encryptPassword = jasyptEncrypt(password);
+
+        System.out.println("encryptUrl : " + encryptUrl);
+        System.out.println("encryptUsername : " + encryptUsername);
+        System.out.println("encryptPassword" + encryptPassword);
+
+        Assertions.assertThat(url).isEqualTo(jasyptDecryt(encryptUrl));
     }
+
+    private String jasyptEncrypt(String input) {
+        String key="";
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.encrypt(input);
+    }
+
+    private String jasyptDecryt(String input){
+        String key="";
+
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithMD5AndDES");
+        encryptor.setPassword(key);
+        return encryptor.decrypt(input);
+    }
+
 
 }
