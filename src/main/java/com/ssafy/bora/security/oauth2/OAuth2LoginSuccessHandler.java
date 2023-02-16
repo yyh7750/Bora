b51fb7d945f371da1439ef9368b68fc823788f74
김대은 <eodms090@naver.com>
2023-02-16 17:05:14 +0900

    chore: test

--- src/main/java/com/ssafy/bora/security/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/ssafy/bora/security/oauth2/OAuth2LoginSuccessHandler.java
@@ -95,7 +95,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
             if (oAuth2User.getRole() == Role.GUEST) {
                 log.info("GUEST");
                 // http://localhost:3000/signup?token={accessToken}
-                targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/regist")
+                targetUrl = UriComponentsBuilder.fromUriString("http://localhost:3000/regist")
                         .queryParam("token", accessToken)
                         .build().toUriString();
                 log.info("targetUrl : " + targetUrl);
